0d686e227abd5e6cc3fcc795d97d452ce944d2dc
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-10-24 12:13:35 +0700

    Quick Sort

--- main.cpp
+++ main.cpp
@@ -1,71 +1,93 @@
+
+#include <sstream>
 #include <iostream>
+#include <type_traits>
 using namespace std;
+
+template<class T>
+void swap(T* a, T* b){
+    // cout << "Before: " << *a << " " << *b << endl;
+    T temp = *a;
+    *a = *b;
+    *b = temp;
+    // cout << "After: " << *a << " " << *b << endl;
+
+}
+template<class T>
+void printArray(T* start, T* end){
+    for (int i = 0; i < end - start; i++) cout << *(start + i) << " ";
+    cout << endl;
+}
+
 
 template <class T>
 class Sorting
 {
 private:
-    static void printArray(T *start, T *end)
-    {
-        int size = end - start;
-        for (int i = 0; i < size; i++)
-            cout << start[i] << " ";
-        cout << endl;
+    static T *Partition(T *start, T *end){
+        if (start == end - 1) return start;
+        if (start == end - 2){
+            // cout << "HAHAA";
+        }
+        T* p = start;
+        T* l = start + 1;
+        T* r = end - 1;
+        // cout << l - start << " " << r - start << endl;
+
+        while(true){
+            while(l <= r){
+                if(*l >= *p) break;
+                l++;
+            }
+            // cout << l - start << endl;
+            while(l<=r){
+                if(*r<*p)break;
+                r--;
+            }
+            // cout << r - start << endl;
+            if (l>r) {
+                swap(p,r);
+                p = r;
+                break;
+            }
+            swap(l,r);
+            l++;
+            r--;
+        }
+        // cout << "Pivot Index: " << p - start << endl;
+        return p;
     }
 
 public:
-    // TODO: Write your code here
-    static void sortSegment(T *start, T *end, int segment_idx, int cur_segment_total)
-    {
-        int arrSize = end - start;
-        int num_elements = 0;
-        for (int i = segment_idx; i < arrSize; i += cur_segment_total)
-        {
-            // cout << i << " ";
-            num_elements++;
+    static void QuickSort(T *start, T *end, int first_index = 0){
+        // cout << "HAHAHA " << *Partition(start, end);
+        // Partition(start, end);
+        if (start == end - 1) {
+            cout << 0 << " ";
+            return;
         }
-        // cout << num_elements << endl;
-        for (int i = 0; i < num_elements - 1; i++)
-        {
-            for (int j = 0; j < num_elements - i - 1; j++)
-            {
-                int first_index = segment_idx + cur_segment_total * j;
-                int second_index = segment_idx + cur_segment_total * (j + 1);
-                if (start[first_index] > start[second_index])
-                {
-                    // cout << start[first_index] << " " << start[second_index] << endl;
-                    int temp = start[first_index];
-                    start[first_index] = start[second_index];
-                    start[second_index] = temp;
-                }
-            }
-        }
-        
 
-    }
-    static void ShellSort(T *start, T *end, int *num_segment_list, int num_phases)
-    {
-        for (int i = num_phases - 1; i >= 0; i--){
-            for (int j = 0; j < num_segment_list[i]; j++){
-                sortSegment(start, end, j, num_segment_list[i]);
-            }
-            cout << num_segment_list[i] << " segments: ";
-            printArray(start, end);
-        }
+
+        if (start == end) return;
+        T* p = Partition(start, end);
+        cout << p - start  << " ";
+        QuickSort(start, p, 0);
+        QuickSort(p+1, end, p - start + 1);
+
+
     }
 };
 
 void test()
 {
-    int num_segment_list[] = {1, 3, 5};
-    int num_phases = 3;
-    int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
-    Sorting<int>::ShellSort(&array[0], &array[10], &num_segment_list[0], num_phases);
+    int array[] = { 3, 5, 7, 10 ,12, 14, 15, 13, 1, 2, 9, 6, 4, 8, 11, 16, 17, 18, 20, 19 };
+cout << "Index of pivots: ";
+Sorting<int>::QuickSort(&array[0], &array[20]);
+cout << "\n";
+cout << "Array after sorting: ";
+for (int i : array) cout << i << " ";
 }
-
-int main()
-{
+int main (){
     test();
     return 0;
 }
